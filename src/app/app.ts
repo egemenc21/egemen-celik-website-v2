08a71d7f7b51c28185ebbf41296614ed6cc4471d
egemenc <egemenc2101@gmail.com>
2025-10-11 16:59:28 +0300

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('egemen-celik-website-v2-css');
+}
