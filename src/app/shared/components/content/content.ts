bf2f8e509908f03b6bf635d6dd7bc9a388580a04
egemenc <egemenc2101@gmail.com>
2025-10-12 20:05:44 +0300

    feat: enhance content component with animations and responsive design; update navigation styles and add content image

--- src/app/shared/components/content/content.ts
+++ src/app/shared/components/content/content.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import {NgClass, NgOptimizedImage} from '@angular/common';
+import {AnimateOnViewDirective} from '../../directives/animate-on-view.directive';
 
 @Component({
   selector: 'app-content',
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    AnimateOnViewDirective,
+    NgClass
+  ],
   templateUrl: './content.html',
   styleUrl: './content.css'
 })
 export class Content {
+  protected isContentVisible: boolean = false;
+  protected isUlVisible: boolean = false;
 
+  isContentVisibleChange(visible: boolean) {
+   this.isContentVisible = visible;
+  }
+
+  isUlVisibleChange(visible: boolean) {
+   this.isUlVisible = visible;
+  }
 }
