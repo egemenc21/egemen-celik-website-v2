72a0e684d10e3fbdfe1f356c90b48c6661394961
Egemen Celik <egemenc2101@gmail.com>
2025-10-16 10:07:02 +0300

    feat: add animations to about me page

--- src/app/shared/components/about-me/about-me.component.ts
+++ src/app/shared/components/about-me/about-me.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
+import {NgClass, NgOptimizedImage} from '@angular/common';
+import {AnimateOnViewDirective} from '../../directives/animate-on-view.directive';
 
 @Component({
   selector: 'app-about-me',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    AnimateOnViewDirective,
+    NgClass
   ],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
 export class AboutMeComponent {
+  isAboutMeVisible: boolean = false;
+  isAboutMeParagraphVisible: boolean = false;
+  onAboutMeVisibleChange(event:boolean) {
+    this.isAboutMeVisible = event;
+  }
 
+  onAboutMeParagraphVisibleChange(event: boolean) {
+    this.isAboutMeParagraphVisible = event;
+  }
 }
