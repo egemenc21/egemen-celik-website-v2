fff878399260e37e78df9d76577aaf1c182427c5
egemenc <egemenc2101@gmail.com>
2025-10-29 19:47:18 +0300

    refactor: change of images, projects, skills and contact pages

--- src/app/shared/components/projects/projects.component.ts
+++ src/app/shared/components/projects/projects.component.ts
@@ -1,14 +1,79 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
+import {CommonModule, NgForOf, NgIf, NgOptimizedImage} from '@angular/common';
+import { LucideAngularModule, Github, ExternalLink } from 'lucide-angular';
+
+interface Project {
+  image: string;
+  alt: string;
+  width: number;
+  height: number;
+  rowSpan: string;
+  objectPosition?: string;
+  githubUrl: string;
+  liveUrl?: string;
+}
+
 
 @Component({
   selector: 'app-projects',
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage, LucideAngularModule, CommonModule
   ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+  // Icons
+  GithubIcon = Github;
+  ExternalLinkIcon = ExternalLink;
+
+  // Projects data
+  projects: Project[] = [
+    {
+      image: 'assets/images/campTraveller.png',
+      alt: 'Camp Traveller',
+      width: 1920,
+      height: 4879,
+      rowSpan: 'row-span-6',
+      githubUrl: 'https://github.com/egemenc21/camptraveler',
+      liveUrl: 'https://camptraveler-sand.vercel.app/'
+    },
+    {
+      image: 'assets/images/crwn-clothing.png',
+      alt: 'Crown Clothing',
+      width: 1850,
+      height: 1574,
+      rowSpan: 'row-span-4',
+      githubUrl: 'https://github.com/egemenc21/crwn-clothing-e-commerce',
+      liveUrl: 'https://fanciful-syrniki-5664a9.netlify.app/'
+    },
+    {
+      image: 'assets/images/mcp-irail-image.png',
+      alt: 'MCP iRail',
+      width: 732,
+      height: 897,
+      rowSpan: 'row-span-3',
+      objectPosition: 'object-top',
+      githubUrl: 'https://github.com/egemenc21/i-rail-mcp',
+    },
+    {
+      image: 'assets/images/tricountReplica.png',
+      alt: 'Tricount Replica',
+      width: 1017,
+      height: 969,
+      rowSpan: 'row-span-3',
+      objectPosition: 'object-top',
+      githubUrl: 'https://github.com/egemenc21/tricount-replica-app',
+      liveUrl: 'https://main--aquamarine-empanada-f3b8ca.netlify.app/'
+    },
+    {
+      image: 'assets/images/mern-chat.png',
+      alt: 'MERN Chat',
+      width: 1919,
+      height: 1080,
+      rowSpan: 'row-span-2',
+      githubUrl: 'https://github.com/egemenc21/mern-chat'
+    }
+  ];
 
 }
--- src/app/shared/components/skills/skills.component.ts
+++ src/app/shared/components/skills/skills.component.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 
+interface SkillCategory {
+  title: string;
+  image: string;
+  alt: string;
+  width: number;
+  height: number;
+  skills: string[];
+}
+
+
 @Component({
   selector: 'app-skills',
     imports: [
@@ -10,5 +20,30 @@ import {NgOptimizedImage} from "@angular/common";
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
-
+  skillCategories: SkillCategory[] = [
+    {
+      title: 'LANGUAGES',
+      image: 'assets/images/safar-safarov-koOdUvfGr4c-unsplash.jpg',
+      alt: 'Programming Languages',
+      width: 1600,
+      height: 2400,
+      skills: ['C#', 'JavaScript', 'TypeScript', 'HTML', 'CSS']
+    },
+    {
+      title: 'FRAMEWORKS',
+      image: 'assets/images/arnold-francisca-f77Bh3inUpE-unsplash.jpg',
+      alt: 'Development Frameworks',
+      width: 1600,
+      height: 2400,
+      skills: ['ASP.NET Core', 'React', 'Angular', 'Node.js', 'Tailwind']
+    },
+    {
+      title: 'DATABASE & TOOLS',
+      image: 'assets/images/matthieu-beaumont-iYnpYeyu57k-unsplash.jpg',
+      alt: 'Database and Development Tools',
+      width: 1600,
+      height: 2400,
+      skills: ['MSSQL', 'MongoDB', 'Git', 'AWS', 'Docker', 'CI/CD', 'Agile']
+    }
+  ];
 }
